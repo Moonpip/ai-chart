ae6d4a5f25082d58718c621ec4254e6de0f5fc95
yourname <you@example.com>
2026-04-09 23:25:23 +0900

    SW追加

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,7 @@
-const CACHE_NAME = "ai-chart-v1";
-
-const urlsToCache = [
-  "./",
-  "./ima.html",
-  "./manifest.json",
-  "./js/",
-  "./generated/",
-  "./final_json/"
-];
-
-// インストール
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", e => {
+  console.log("SW install");
 });
 
-// リクエスト時
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", e => {
+  // 何もしなくてOK（最低限）
 });
